24fd12b6bd862e7690a401fb59655ef76901031d
maczhang <zhangfan824@163.com>
2017-08-27 07:06:45 +0800

    demo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+class Welcome extends React.Component { 
+  render() {
+    return <h1>Hello, Component</h1>;
+  }
+}
+
 ReactDOM.render(
-    <h1>Hello, frank!</h1>,
-    document.getElementById('root2')
-  );
+  <Welcome/>,
+  document.getElementById('root')
+)
