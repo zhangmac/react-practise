52a6104932cecbe43fcfd96626bc94f7bbe2ae18
maczhang <zhangfan824@163.com>
2017-08-29 20:01:03 +0800

    outline

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state= {
+      newTodo:"test",
+      todoList:[
+        {id:1,title:"go to winterfell"}
+      ]
+    }
+  }
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    let todos = this.state.todoList.map((item,index)=>{
+      return <li>{item.title}</li>
+    })
+    return(
+    <div className="App">
+        <h1>My projects</h1>
+      <div className="inputWrapper">
+        <input type="text" value={this.state.newTodo}/>
       </div>
-    );
+      <ol>
+      {todos}
+    </ol>
+    </div>
+    ) 
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,15 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-class Welcome extends React.Component { 
-  render() {
-    return <h1>Hello, Component</h1>;
-  }
-}
 
 ReactDOM.render(
-  <Welcome/>,
+  <App />,
   document.getElementById('root')
 )
